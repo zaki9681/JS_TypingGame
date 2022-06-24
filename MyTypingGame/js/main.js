d87bab8c375951e2413b93acd1c38ff51cd12691
zaki9681 <ryui.hamazaki@gmail.com>
2022-06-24 16:52:28 +0900

    スペースキーでゲームを開始できるよう修正しました

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -16,10 +16,21 @@
     
     let word = 'red';
     let loc = 0;
-    
+    let startTime;
+    let isPlaying = false;
     const target = document.getElementById('target');
     
-    setWord();
+    document.addEventListener('keydown', e => {
+        if(isPlaying === true){
+            return;
+        }
+        
+        if(e.key === ' ') {
+            isPlaying = true;
+            startTime = Date.now();
+            setWord();
+        }  
+    })
     
     
     document.addEventListener('keydown', e => {
@@ -38,6 +49,12 @@
         target.textContent = '_'.repeat(loc) + word.substring(loc);
         
         if(loc === word.length) {
+            if(words.length === 0) {
+                const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+                const result = document.getElementById('result');
+                result.textContent = `Finished! ${elapsedTime} seconds!` ;
+                return;   
+            }
             setWord();
         }
     })
